9b260d0248ecf406fa9c1777762f1b530a029d45
Lou Zell <lzell11@gmail.com>
2013-06-16 06:29:10 -0700

    OKCloud now uses OKNetworker to perform all REST calls

--- OpenKit/OKCloud.m
+++ OpenKit/OKCloud.m
@@ -15,49 +15,13 @@
 // Redis cli:
 // hget "dev:1:user:11" "firstKey"
 
-#define USE_JSONKIT  1
-
 
 #import "OKCloud.h"
 #import "OKUser.h"
-#import "OKCloudAsyncRequest.h"
-#import "JSONKit.h"
 #import "OKMacros.h"
 #import "OKError.h"
-
-static void
-encodeObj(id obj, NSString **strOut, NSError **errOut)
-{
-#if USE_JSONKIT
-    JKSerializeOptionFlags opts = JKSerializeOptionNone;
-    if ([obj isKindOfClass:[NSString class]]) {
-        *strOut = [obj JSONStringWithOptions:opts includeQuotes:YES error:errOut];
-    }
-    else {
-        *strOut = [obj JSONStringWithOptions:opts error:errOut];
-    }
-    NSLog(@"Json is: %@", *strOut);
-#else
-    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:obj options:NULL error:errOut];
-    if (!*errOut) {
-        *strOut = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-        NSLog(@"Json is: %@", *strOut);
-    }
-#endif
-}
-
-static id
-decodeObj(NSData *dataIn, NSError **errOut)
-{
-#if USE_JSONKIT
-    JSONDecoder *decoder = [JSONDecoder decoderWithParseOptions:JKParseOptionNone];
-    id obj = [decoder objectWithData:dataIn error:errOut];
-    return obj;
-#else
-    NSJSONReadingOptions opts = NSJSONReadingAllowFragments | NSJSONReadingMutableContainers | NSJSONReadingMutableLeaves;
-    return [NSJSONSerialization JSONObjectWithData:dataIn options:opts error:errOut];
-#endif
-}
+#import "OKUtils.h"
+#import "OKNetworker.h"
 
 
 @implementation OKCloud
@@ -66,31 +30,24 @@ decodeObj(NSData *dataIn, NSError **errOut)
 + (void)set:(id)obj key:(NSString *)key completion:(void (^)(id obj, NSError *err))completion
 {
     OKUser *user = [OKUser currentUser];
-    
+
     if(user == nil) {
         completion(nil, [OKError userNotLoggedInError]);
         return;
     }
     
-    NSError *err;
-    NSString *objRep;
-
-    encodeObj(obj, &objRep, &err);
-
+    // AFNetworking will handle encode of 'obj'...
     NSDictionary *params = [NSDictionary dictionaryWithObjectsAndKeys:
                             user.OKUserID,  @"user_id",
                             key,            @"field_key",
-                            objRep,         @"field_value",
+                            obj,            @"field_value",
                             nil];
 
-    OKCloudAsyncRequest *req = [[OKCloudAsyncRequest alloc] initWithPath:@"/developer_data"
-                                                 requestMethod:@"POST"
-                                                    parameters:params];
-
-    [req performWithCompletionHandler:^(id responseObj, NSError *err) {
+    [OKNetworker postToPath:@"/developer_data" parameters:params handler:^(id responseObj, NSError *err) {
         completion(obj, err);
     }];
 }
+
 
 + (void)get:(NSString *)key completion:(void (^)(id obj, NSError *err))completion
 {
@@ -105,22 +62,12 @@ decodeObj(NSData *dataIn, NSError **errOut)
                             nil];
 
     NSString *path = [NSString stringWithFormat:@"/developer_data/%@", key];
-    OKCloudAsyncRequest *req = [[OKCloudAsyncRequest alloc] initWithPath:path
-                                                 requestMethod:@"GET"
-                                                    parameters:params];
-
-    [req performWithCompletionHandler:^(id responseObj, NSError *err) {
-#ifdef DEBUG
-        OKLog(@"OKCloud Response: %@", [[NSString alloc] initWithData:responseObj encoding:NSUTF8StringEncoding]);
-#endif
+    [OKNetworker getFromPath:path parameters:params handler:^(id responseDict, NSError *err) {
         id o = nil;
         if (!err) {
-            NSDictionary *dict = decodeObj(responseObj, &err);
-            if (!err) {
-                o = [dict objectForKey:key];
-                if ([o isKindOfClass:[NSNull class]]) {
-                    o = nil;
-                }
+            o = [responseDict objectForKey:key];
+            if ([o isKindOfClass:[NSNull class]]) {
+                o = nil;
             }
         }
         completion(o, err);
