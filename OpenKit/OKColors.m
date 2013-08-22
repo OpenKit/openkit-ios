6e719f56a4808b9b80b5f65cdaa2e15be367fab2
Todd Hamilton <toddham@gmail.com>
2013-08-22 13:34:30 -0700

    color changes

--- OpenKit/OKColors.m
+++ OpenKit/OKColors.m
@@ -9,7 +9,6 @@
 
 +(UIColor*)navbarTintColor {
     return UIColorFromRGB(0xe8e8e8);
-    //return [UIColor colorWithRed:240.0/255.0 green:241.0/255.0 blue:243.0/255.0 alpha:1.0];
 }
 
 +(NSDictionary*)titleTextAttributesForNavBarButton {
