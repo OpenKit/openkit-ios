232387bbc49704e77fc76d12512a7f4a6795c63e
Todd Hamilton <toddham@gmail.com>
2013-08-22 12:52:07 -0700

    trying out new tint color

--- OpenKit/OKColors.m
+++ OpenKit/OKColors.m
@@ -4,11 +4,12 @@
 @implementation OKColors
 
 +(UIColor*)navbarTextColor {
-    return [UIColor colorWithRed:77.0/255.0 green:77.0/255.0 blue:77.0/255.0 alpha:1.0] ;
+    return [UIColor colorWithRed:77.0/255.0 green:77.0/255.0 blue:77.0/255.0 alpha:1.0];
 }
 
 +(UIColor*)navbarTintColor {
     return UIColorFromRGB(0xe8e8e8);
+    //return [UIColor colorWithRed:240.0/255.0 green:241.0/255.0 blue:243.0/255.0 alpha:1.0];
 }
 
 +(NSDictionary*)titleTextAttributesForNavBarButton {
