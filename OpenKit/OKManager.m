a29a609b25b3b6ee9c34efb0a77c414c260902fd
Suneet Shah <suneets@gmail.com>
2013-10-04 00:56:42 -0700

    set version tag to 1.0.3, minor bug fixes

--- OpenKit/OKManager.m
+++ OpenKit/OKManager.m
@@ -21,7 +21,7 @@
 #import "OKMacros.h"
 
 #define OK_DEFAULT_ENDPOINT    @"http://api.openkit.io"
-#define OK_OPENKIT_SDK_VERSION = @"1.0.2";
+#define OK_OPENKIT_SDK_VERSION = @"1.0.3";
 
 static NSString *OK_USER_KEY = @"OKUserInfo";
 
