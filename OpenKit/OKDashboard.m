d93cd424cb4ef1d4a0748b18700b1f2bc28c56f1
Suneet Shah <suneets@gmail.com>
2013-10-17 17:44:11 -0700

    set tab bar images

--- OpenKit/OKDashboard.m
+++ OpenKit/OKDashboard.m
@@ -17,6 +17,13 @@
     OKLeaderboardsViewController *leaderboards = [[OKLeaderboardsViewController alloc] init];
     OKChatViewController *chat = [[OKChatViewController alloc] init];
     
+    
+    UITabBarItem *leaderboardsItem = [[UITabBarItem alloc] initWithTitle:@"Leaderboards" image:[UIImage imageNamed:@"leaderboards.png"] selectedImage:[UIImage imageNamed:@"leaderboards_active.png"]];
+    [leaderboards setTabBarItem:leaderboardsItem];
+    
+    UITabBarItem *chatItem = [[UITabBarItem alloc] initWithTitle:@"Chat" image:[UIImage imageNamed:@"chat.png"] selectedImage:[UIImage imageNamed:@"chat_active.png"]];
+    [chat setTabBarItem:chatItem];
+    
     NSArray *viewControllers = [NSArray arrayWithObjects:leaderboards, chat, nil];
     
     UITabBarController *tabBar = [[UITabBarController alloc] init];
